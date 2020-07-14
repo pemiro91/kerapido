a8f5871468288616d382cfd654474f0ea575b04f
Pablo Miro <pemiro91@gmail.com>
2020-07-14 02:04:13 -0400

    Update Api

--- kerapido/admin.py
+++ kerapido/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from kerapido.models import Plato, User, Reservacion_Simple
+
+admin.site.register(User)
+admin.site.register(Plato)
+admin.site.register(Reservacion_Simple)
--- kerapido/models.py
+++ kerapido/models.py
@@ -1,8 +1,9 @@
-from django.db import models
-import uuid
-from .validators import validate_file_extension
-from django.contrib.auth.models import AbstractUser
 import datetime
+import uuid
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
 from Ke_Rapido import settings
 
 ESTADO_ENTREGA = (
@@ -13,11 +14,11 @@ ESTADO_ENTREGA = (
 
 
 # Create your models here.
-def images_directory_path(instance, filename):
-    return '/'.join(['images', str(instance.title), str(uuid.uuid4().hex + ".png")])
+# def images_directory_path(instance, filename):
+#     return '/'.join(['images', str(instance.title), str(uuid.uuid4().hex + ".png")])
 
 
-class Negocio(AbstractUser):
+class User(AbstractUser):
     logo = models.ImageField(upload_to='logo/%Y/%m/%d', null=True, blank=True)
     persona_encargada = models.CharField(max_length=255)
     telefono = models.CharField(max_length=255)
@@ -25,35 +26,53 @@ class Negocio(AbstractUser):
     especialidad = models.CharField(max_length=255)
     provincia = models.CharField(max_length=255)
     municipio = models.CharField(max_length=255)
-    localidad = models.CharField(max_length=255)
-    rating = models.FloatField(max_length=20)
+    rating = models.FloatField(max_length=20, null=True, blank=True)
+    is_cliente = models.BooleanField(default=False)
+    is_negocio = models.BooleanField(default=False)
+    is_administrador = models.BooleanField(default=False)
 
 
-class Producto(models.Model):
+class Plato(models.Model):
+    imagen = models.ImageField(upload_to='imagen_plato/%Y/%m/%d', null=True, blank=True)
     nombre = models.CharField(max_length=255)
-    tipo_producto = models.CharField(max_length=255)
-    agregos = models.CharField(max_length=255)
-    precio = models.FloatField(max_length=50)
+    descripcion = models.CharField(max_length=255)
+    precio = models.CharField(max_length=255)
+    negocio = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='negocio')
+
+    def __str__(self): return str(self.nombre)
+
+
+class Tarifa(models.Model):
+    lugar_destino = models.CharField(max_length=255)
+    precio = models.FloatField(max_length=55)
     negocio = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, name='negocio')
 
-    def __str__(self):
-        return self.nombre
 
-
-class Fotos(models.Model):
-    producto = models.ForeignKey(Producto, on_delete=models.CASCADE, name='producto', related_name='photos')
-    file = models.FileField(upload_to='photos/%Y/%m/%d', validators=[validate_file_extension])
-    uploaded_at = models.DateTimeField(auto_now_add=True)
-
-
-class Reservation(models.Model):
-    producto = models.ForeignKey(Producto, on_delete=models.CASCADE, name='producto')
+class Reservacion_Simple(models.Model):
     cantidad_producto = models.IntegerField(name='cantidad_producto')
-    fecha_reservacion = models.DateField(default=datetime.datetime.now, name='fecha_reservacion')
+    plato = models.ForeignKey(Plato, on_delete=models.CASCADE, name='plato')
+
+    def __str__(self): return str(self.plato)
+
+
+class Reservacion_Generada(models.Model):
+    cliente_auth = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, name='cliente_auth')
+    total = models.FloatField(max_length=255)
+    fecha_reservacion = models.DateField(default=datetime.date.today)
+    cliente_entrega = models.CharField(max_length=255)
+    telefono_entrega = models.CharField(max_length=255)
+    direccion_entrega = models.CharField(max_length=255)
+    reservaciones = models.ManyToManyField(Reservacion_Simple)
+
+
+class Agrego(models.Model):
+    nombre = models.CharField(max_length=255)
+    precio = models.FloatField(max_length=255)
+    negocio = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, name='negocio')
 
 
 class Estado_Reservation(models.Model):
-    reservacion = models.ForeignKey(Reservation, on_delete=models.CASCADE, name='reservacion')
+    reservacion = models.ForeignKey(Reservacion_Generada, on_delete=models.CASCADE, name='reservacion')
     estado = models.CharField(max_length=50, choices=ESTADO_ENTREGA, name='estado')
     fecha_estado = models.DateField()
     negocio = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, name='negocio')
