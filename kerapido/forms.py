1502737453917f861137a9ea88a1185c5df94d0b
Pablo Miro <pemiro91@gmail.com>
2020-09-05 00:15:03 -0400

    Update Forms

--- kerapido/forms.py
+++ kerapido/forms.py
@@ -1,12 +1,30 @@
 from django import forms
-from .models import Provincia, Municipio, Negocio
+
+from kerapido.models import Negocio
 
 
-class ProvinciaForm(forms.ModelForm):
+class RegisterProduct(forms.Form):
+    imagen = forms.ImageField(label='Imagen')
+    name_product = forms.CharField(label='Nombre del producto*', max_length=15, widget=forms.TextInput(
+        attrs={'class': 'form-control text',
+               'placeholder': 'Escriba el nombre del producto',
+               }), required=True)
+    description_product = forms.Textarea(attrs={'class': 'form-control no-resize',
+                                                'placeholder': 'Escriba la descripción del producto',
+                                                })
+    price_product = forms.CharField(label='Precio*', max_length=15, widget=forms.TextInput(
+        attrs={'class': 'form-control text',
+               'placeholder': 'Escriba el precio del producto',
+               }), required=True)
+
+
+class MyForm(forms.ModelForm):
     class Meta:
         model = Negocio
-        fields = ('nombre', 'birthdate', 'country', 'city')
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['provincia'].queryset = Municipio.objects.none()
+        fields = ['nombre', 'logo', 'portada', 'eslogan', 'categorias', 'servicios']
+        exclude = ['usuario_negocio']
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombre', }),
+            'eslogan': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '', }),
+            'categorias': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '', }),
+        }
